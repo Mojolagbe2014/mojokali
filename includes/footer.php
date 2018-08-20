8b1636291cab9a4db908564a7811e323d8b4a1de
jamiumojolagbe <jamiumojolagbe@192.168.0.70>
2018-08-19 19:04:34 -0700

    developer details added

--- includes/footer.php
+++ includes/footer.php
@@ -3,13 +3,15 @@
                 <div class="copyright_wrap_inner">
                     <div class="content_wrap">
                         <div class="sc_socials sc_socials_type_icons sc_socials_shape_square sc_socials_size_tiny">
-                            <div class="sc_socials_item">
-                                <a href="<?php echo LINKEDIN_LINK; ?>" target="_blank" class="social_icons social_linkedin"><span class="icon-linkedin"></span></a>
-                            </div>
+                            
                             <div class="sc_socials_item">
                                 <a href="<?php echo FACEBOOK_LINK; ?>" target="_blank" class="social_icons social_facebook">
                                     <span class="icon-facebook"></span>
                                 </a>
+                            </div>
+                            <?php if(1==2) { ?>
+                            <div class="sc_socials_item">
+                                <a href="<?php echo LINKEDIN_LINK; ?>" target="_blank" class="social_icons social_linkedin"><span class="icon-linkedin"></span></a>
                             </div>
                             <div class="sc_socials_item">
                                 <a href="<?php echo TWITTER_LINK; ?>" target="_blank" class="social_icons social_twitter">
@@ -21,8 +23,9 @@
                                     <span class="icon-gplus"></span>
                                 </a>
                             </div>
+                            <?php } ?>
                         </div>							
-                        <div class="copyright_text"><p>&copy; <?php $currYear   = new DateTime(); echo $currYear->format('Y'); ?> All Rights Reserved</p></div>
+                        <div class="copyright_text"><p>&copy; <?php $currYear   = new DateTime(); echo $currYear->format('Y'); ?> All Rights Reserved. &nbsp; Developed by: <a href="http://http://www.timcacomputerinc.net/" target="_blank"> TIMCA Computer Inc</a></p></div>
                     </div>
                 </div>
             </div>
